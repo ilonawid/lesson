b94d3ae370d1c032adc033ad9b5faa55ac338dd5
ilonawid <ilonasirokaa87270@gmail.com>
2024-09-30 23:48:27 +0300

    Add files via upload

---
+++ module1_1.py
@@ -0,0 +1,6 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = 15
+course = 'Python'
+time_per_task = number_of_hours_spent/number_of_completed_tasks
+print('Курс:', course, 'всего задач:', number_of_completed_tasks, 'затрачено часов:', number_of_hours_spent, 'среднее время выполнения:', time_per_task, 'часа'
+                                                                                                                                                         '')
