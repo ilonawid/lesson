a4cfda7eebbfd1c89cb3833575a49cd540cc5207
ilonawid <ilonasirokaa87270@gmail.com>
2024-10-02 23:01:11 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 1, 'осень', True
+print(immutable_var)
+# отличие кортежа от списка - нельзя изменить значения элементов.
+mutable_list = [1, 2, 3]
+mutable_list[0] = 5
+print(mutable_list)
