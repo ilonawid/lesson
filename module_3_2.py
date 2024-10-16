6463141a3de324c37565bce28301b2d3281c7582
ilonawid <ilonasirokaa87270@gmail.com>
2024-10-16 23:57:32 +0300

    H/W

---
+++ module_3_2.py
@@ -0,0 +1,25 @@
+import re
+
+
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    if sender == 'university.help@gmail.com':
+        print('Письмо успешно отправлено с адреса ', sender, ' на адрес ', recipient)
+    elif '@' not in recipient and sender:
+        print('Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient)
+    elif not recipient.endswith(('.com', '.ru', '.net')):
+        print('Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient)
+    elif not sender.endswith(('.com', '.ru', '.net')):
+        print('Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient)
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    else:  # recipient != 'university.help@gmail.com'
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ', sender, ' на адрес ', recipient)
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
