cda4aecd2ba7bb5d5d39621bba17b677c99e349c
ilonawid <ilonasirokaa87270@gmail.com>
2024-11-12 20:58:20 +0300

    H/W

---
+++ module_6_1.py
@@ -0,0 +1,56 @@
+class Animal:
+    alive = True  # живой
+    fed = False  # накормленный
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Plant:
+    edible = False  # съедобность
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Mammal(Animal):
+    def eat(self, food):
+        if type(food) == Fruit:
+            print(f'{self.name} сьел {food.name}')
+            fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            alive = False
+
+
+class Predator(Animal):
+    def eat(self, food):
+        if type(food) == Fruit:
+            fed = True
+            print(f'{self.name} сьел {food.name}')
+
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            alive = False
+
+
+class Fruit(Plant):
+    edible = True
+
+class Flower(Plant):
+    pass
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
