0ed343a247f9db5ead4f426f12052070074f2dc7
ilonawid <ilonasirokaa87270@gmail.com>
2024-12-09 23:50:06 +0300

    H/W

---
+++ module_9_5.py
@@ -0,0 +1,65 @@
+class StepValueError(ValueError):
+    pass
+
+
+class Iterator():
+    def __init__(self, start, stop, step=1):
+        self.start = start  # - целое число, с которого начинается итерация.
+        self.stop = stop  # - целое число, на котором заканчивается итерация.
+        self.step = step  # шаг, с которым совершается итерация.
+        self.pointer = self.start  # указывает на текущее число в итерации (изначально start)
+        if self.step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        self.pointer += self.step
+        if self.step > 0:
+            if self.pointer <= self.stop:
+                return self.pointer
+        else:
+            if self.pointer >= self.stop:
+                return self.pointer
+        raise StopIteration()
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+try:
+    iter2 = Iterator(-5, 1)
+    for i in iter2:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
+
+try:
+    iter3 = Iterator(6, 15, 2)
+    for i in iter3:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
+
+try:
+    iter4 = Iterator(5, 1, -1)
+    for i in iter4:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
+
+try:
+    iter5 = Iterator(10, 1)
+    for i in iter5:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
